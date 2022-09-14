dc215f1c46a85518a9ad1ddc840b27b6e3f7ab9b
Elliot Su <sodinfeliz@gmail.com>
2022-09-14 15:51:50 +0800

    Update workbook 2.2

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -2,18 +2,26 @@ public class Biography {
     public static void main(String[] args) {
 
         //make a name variable
+        String name = "John";
         //make an age variable
+        int age = 22;
         //make a country variable
+        String country = "NYC";
         //make a sport variable
+        String sport = "baseball";
         //make an hours variable
+        int hours = 3;
         //make a game variable
+        String game = "Switch";
         //make a subject variable
+        String subject = "Math";
         //make a grade variable
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
 
     }
 }
